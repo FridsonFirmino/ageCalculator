022f98144a0c3cc302748429a74ea9749d26bdae
fridsonfirmnio <fridsoncmf@gmail.com>
2024-03-16 22:01:04 +0100

    feat: add nativewind style in home page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 export default function Home(){
   return (
     <View className="flex-1 bg-slate-950 items-center justify-center">
-      <Text className="text-white">Home</Text>
+      <Text className="text-white">HomeScreen</Text>
     </View>
   )
 }
