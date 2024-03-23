e6e08a906b67ec2c4b9ef87ad495cdbf411b9b76
fridsonfirmnio <fridsoncmf@gmail.com>
2024-03-23 20:03:18 +0100

    feat: update home screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,6 +5,7 @@ export default function Home(){
   return (
     <View className="flex-1 bg-slate-950 items-center justify-center">
       <Text className="text-white">HomeScreen</Text>
+      <Text>AGE-CALCULATOR</Text>
     </View>
   )
 }
