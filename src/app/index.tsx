9c6265dc313af70c20cbb281ab6f69017cf4944b
fridsonfirmnio <fridsoncmf@gmail.com>
2024-04-03 21:48:00 +0100

    feat: update home page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -11,6 +11,7 @@ export default function Home(){
       <Text>AGE-CALCULATOR</Text>
       <Text>AGE-CALCULATOR</Text>
       <Text>AGE-CALCULATOR</Text>
+      <Text>AGE-CALCULATOR</Text>
     </View>
   )
 }
